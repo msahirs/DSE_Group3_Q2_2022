23612192c24ff730eb5b869d82dac4c7b4f1be31
ssujahudeen <sujahudeen.sahir@gmail.com>
2022-12-20 14:20:16 +0100

    density spline plotter in

--- Balloon_lift.py
+++ Balloon_lift.py
@@ -1,11 +1,13 @@
 import math
 import matplotlib.pyplot as plt
 from Functions import ISA
+from scipy.interpolate import UnivariateSpline
+import numpy as np
 
 mass = 1000
 dt = 0.1
 
-
+plt.rcParams.update({'font.size': 15, 'xtick.labelsize' : 11, 'ytick.labelsize' : 11})
 
 # print(ISA(10))
 
@@ -20,12 +22,12 @@ def calc_DlDv(rho_gas,rho_atm):
     return -DlDv
 
 
-h_range = [h for h in range(0,45000,100)]
+h_range = [h for h in range(45000,45100, 100)]
 
 DlDv_list = list()
 
 for h in h_range:
-    pass
+    
     atm_params = ISA(h)
     hydrogen_density = relative_density(0.0841,288.15,
                                         atm_params[0])
@@ -34,9 +36,21 @@ for h in h_range:
     DlDv_list.append(calc_DlDv(atm_params[2], hydrogen_density))
 
 
+# y_spl = UnivariateSpline(h_range,DlDv_list,s=0,k=4)
+# x_range = np.linspace(h_range[0],h_range[-1],100)
 
-
-plt.plot(h_range,DlDv_list)
+plt.plot(h_range,DlDv_list,)
+# plt.plot(x_range,y_spl(x_range), label = 'spline fit')
 plt.xlabel("Height [m]")
 plt.ylabel("Lifted mass per volume of gas [kg/m^3]")
+# plt.legend()
 plt.show()
+
+print(DlDv_list)
+
+# y_spl_2d = y_spl.derivative(n=2)
+# print(y_spl_2d(x_range))
+
+# plt.plot(x_range,y_spl_2d(x_range),label = '2nd Derivative')
+# plt.legend()
+# plt.show()
