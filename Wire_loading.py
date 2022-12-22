e58ec5a021146e744381e757412bbd7f58f2e8cf
sleind2001 <ndonners@student.tudelft.nl>
2022-12-22 13:07:13 +0100

    niels: made junk file

--- Wire_loading.py
+++ Wire_loading.py
@@ -283,24 +283,3 @@ coordlst = create_mesh(3)
 # print(gen_stiffness_matrix_element(100, 10 ** -2, 1, [0, 0], [1, 1]))
 # array = np.array([[[1, 0, -1, 0], [0, 0, 0, 0], [-1, 0, 1, 0], [0, 0, 0, 0]]])
 # print(make_global_stiffness_matrix(array))
-
-## set up dataframe for use ##
-
-# mass
-# length_of_segment = total_wire_length / wire_segments
-# volume_of_segment = area_of_segment * length_of_segment
-# mass_of_segment = volume_of_segment * density_of_material
-# mass_list = mass_of_segment * np.ones(wire_segments)
-#
-# # wind/gust
-# wind_profile_list = np.zeros(wire_segments)
-#
-# # initial tension
-# initial_tension_list = np.zeros(wire_segments)
-#
-# # orientation
-# angle_of_orientation_list = np.zeros(wire_segments)
-#
-# # positioning
-# top_position_list = (np.ones(wire_segments), np.ones(wire_segments))  # tuple -> x,y
-# bottom_position_list = (np.ones(wire_segments), np.ones(wire_segments))  # tuple -> x,y
