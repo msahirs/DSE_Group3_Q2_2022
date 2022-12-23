c5487458230e2e4b1db5da3fc5f1c145b41f8570
JelteB <jeltejb@gmail.com>
2022-12-23 11:31:25 +0100

    version 1.2 Working version for cfrp-air sandwich for turbulent flow

--- PV_Thermal.py
+++ PV_Thermal.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 import ISA_general
 
 # Input Values
-L_charac = 1               # Characteristic length [m]
+L_charac = 1                # Characteristic length [m]
+A = 2700                    # Area of solar cell configuration [m2]
 h = 20000.0                 # Height [m]
 v_wind = 5                  # Wind speed [m/s]
 
 # Constants
-Cp = 1000                   # Specific heat capacity of air [J/kg]
+Cp = 1000                   # Specific heat capacity of air [J/kg/K]
 k = 2 * 10 ** (-3)          # Thermal conductivity of air [W/m/K]
 g = 9.81                    # Gravitational acceleration [m/s2]
 sigma = 5.67 * 10 ** (-8)   # Boltzmann constant
 I = 1353                    # Incident solar intensity [W/m2]
-Cp_module = 2900            # Specific heat capacity of PV [J/kg]
 
 # Calculations
 T_air, P, rho, mu = ISA_general.ISA(h)
@@ -26,13 +26,17 @@ beta = 1 / T_air            # Thermal expansion coefficient [1/K] (approx)
 epsilon = 0.9               # Ge substrate emission coefficient [-]
 refl_top = 0.02             # Reflectivity of solar cell cap [-]
 d_contact = 5*10**-6        # Bottom metal contact thickness [m]
-k_contact = 429  # Conductivity of metal contact (Ag) [W/m/K]
+k_contact = 429             # Conductivity of metal contact (Ag) [W/m/K]
+Rho_Ge = 5323               # Ge substrate density
+Cp_Ge = 3200                # Specific heat capacity of  Ge
+d_Ge = 160*10**-6           # Approx thickness Ge substrate
+Cp_module = Rho_Ge * Cp_Ge * d_Ge * A # Heat capacity of PV [J/K]
 
 # Backplane
 Cp_cfrp = 1040              # Specific heat capacity of CFRP [J/kg]
 d_cfrp = 100 * 10 ** (-6)   # Thickness CFRP [m]
-k_cfrp_hor = 250      # Conductivity (hor) of CFRP [W/m/K]
-k_cfrp_ver = 3        # Conductivity (ver) of CFRP [W/m/K]
+k_cfrp_hor = 250            # Conductivity (hor) of CFRP [W/m/K]
+k_cfrp_ver = 3              # Conductivity (ver) of CFRP [W/m/K]
 epsilon_cfrp = 0.88         # CFRP emission coefficient [-]
 
 
@@ -45,67 +49,52 @@ T_list = []
 T_cfrp = T_air
 temp = []
 
-for t in range(0, 10000, dt):
+for t in range(0, 2000, dt):
     t_list.append(t)
     T_list.append(T - 273.15)
 
-    # forced convection
+    # forced convection (turbulent flow)
     Re = v_wind * rho * L_charac / mu
-    Nu_forced = 0.664 * Re ** (1 / 2) * Pr ** (1 / 3)
+    Nu_forced = 0.037 * Re ** (4 / 5) * Pr ** (1 / 3)
     h_forced = Nu_forced * k / L_charac
-    q_forced_conv = h_forced * L_charac ** 2 * (T - T_air)
+    q_forced_conv = h_forced * A * (T - T_air)
 
     # free convection
     Ra = g * beta / nu / alpha * (T - T_air) * L_charac ** 3
     Nu_free = 0.15 * Ra ** (1 / 3)
     h_free = Nu_free * k / L_charac
-    q_free_conv = h_free * L_charac ** 2 * (T - T_air)
+    q_free_conv = h_free * A * (T - T_air)
 
     # emission radiation
-    q_emission = sigma * epsilon * (T ** 4 - T_air ** 4) * L_charac ** 2
+    q_emission = sigma * epsilon * (T ** 4 - T_air ** 4) * A
 
     # direct absorption
     alpha_ab = 0.65  # TBD
     I_ab = I * (1 - refl_top)
-    q_abs = I_ab * alpha_ab * L_charac ** 2
-
-    # energy generation ~advection
-    ff = (2.43 * 16.8) / (2.73 * 17.41)
-    k1 = 10**6
-    q_energygen = ff * I_ab * math.log(k1 * I_ab) / T
-
+    q_abs = I_ab * alpha_ab * A
 
     # conduction to backplane
     T_cfrp = T
 
     # CFRP emission
-    q_emission_cfrp = sigma * epsilon_cfrp * (T ** 4 - T_air ** 4) * L_charac ** 2
+    q_emission_cfrp = sigma * epsilon_cfrp * (T ** 4 - T_air ** 4) * A
 
-    # CFRP forced convection air layer
+    # CFRP forced convection air layer (turbulent flow)
     Pr_cfrp = Pr
     Re_cfrp = Re
-    Nu_cfrp_forced = 0.664 * Re_cfrp ** (1 / 2) * Pr_cfrp ** (1 / 3)
+    Nu_cfrp_forced = 0.037 * Re_cfrp ** (4 / 5) * Pr_cfrp ** (1 / 3)
     h_cfrp_forced = Nu_cfrp_forced * k / L_charac
-    q_cfrp_forced_conv = h_cfrp_forced * L_charac ** 2 * (T_cfrp - T_air)
+    q_cfrp_forced_conv = h_cfrp_forced * A * (T_cfrp - T_air)
 
     # CFRP free convection air layer
     Ra_cfrp = g * beta / nu / alpha * (T_cfrp - T_air) * L_charac ** 3
     Nu_cfrp_free = 0.15 * Ra_cfrp ** (1 / 3)
     h_cfrp_free = Nu_cfrp_free * k / L_charac
-    q_cfrp_free_conv = h_cfrp_free * L_charac ** 2 * (T_cfrp - T_air)
+    q_cfrp_free_conv = h_cfrp_free * A * (T_cfrp - T_air)
 
     # Time step
-    dT = (q_abs - q_forced_conv - q_free_conv - q_emission - q_emission_cfrp - q_cfrp_free_conv - q_cfrp_forced_conv - q_energygen) / Cp_module * dt
+    dT = (q_abs - q_forced_conv - q_free_conv - q_emission - q_emission_cfrp - q_cfrp_free_conv - q_cfrp_forced_conv) / Cp_module * dt
     T = T + dT
-
-print(q_abs, "absorped")
-print(q_forced_conv, "fo conv")
-print(q_free_conv, "fr conv")
-print(q_emission, "emission")
-print(q_emission_cfrp, "cfrp emission")
-print(q_cfrp_free_conv, "cfrp fr conv")
-print(q_cfrp_forced_conv, "cfrp fo conv")
-print(q_energygen, "advection")
 
 
 plt.plot(t_list, T_list)
