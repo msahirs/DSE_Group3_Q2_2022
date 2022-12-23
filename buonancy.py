c81e6e95154024d3799956c48384c1f8c14cca50
nmeijering <niek.meijering01@gmail.com>
2022-12-23 13:25:04 +0100

    buonancy update

--- buonancy.py
+++ buonancy.py
@@ -1,27 +1,72 @@
-import ISA
+from ISA import ISA as I
 import matplotlib.pyplot as plt
 
 
-h = 20000 # height [m]
-ISA = ISA.ISA(h)
-#V_h = 80000 # volume balloon at altitude [m**3]
+h = 0 # height [m]
+
+
 g0 = 9.80665 # gravity constant [kg/m**2]
-rho_a_h = ISA[2] # density [kg/m**3]
-p_gas_h = ISA[1] # pressure at altitude of h2
-d_p_gas = 400 # pressure diff [kg/m**2]
-d_T_gas = 0 # temp diff [K]
-T_a = ISA[0] # ambient temp [K]
+
+
 mass = 5000
 W = mass * g0
 
 
+
 R_gas = 4116 # specific gas constant [J/(kg*K)
 
-V_h = W / (g0  * (rho_a_h - ((p_gas_h + d_p_gas)/(R_gas * (T_a + d_T_gas)))))
-Lift = g0 * V_h * (rho_a_h - ((p_gas_h + d_p_gas)/(R_gas * (T_a + d_T_gas)))) # N
+def Volume_calc(h, temp):
+    ISA = I(h)
+    rho_a_h = ISA[2]  # density [kg/m**3]
+    p_gas_h = ISA[1]  # pressure at altitude of h2
+    Pd = 0.5 * rho_a_h * 50 ** 2  # dynamic pressure
+    d_p_gas = 2 * Pd  # pressure diff ( twice dynamic press) [kg/m**2]
+    d_T_gas = temp # temp diff [K]
+    T_a = ISA[0]  # ambient temp [K]
 
-lift_kg = Lift /g0
-print(p_gas_h)
-print(rho_a_h)
-print(round(V_h, 3), "m3")
-print(V_h/2600)
+    V_h = W / (g0 * (rho_a_h - ((p_gas_h + d_p_gas) / (R_gas * (T_a + d_T_gas)))))
+
+    return V_h
+
+def Lift_calc(h, V, temp):
+    ISA = I(h)
+    rho_a_h = ISA[2]  # density [kg/m**3]
+    p_gas_h = ISA[1]  # pressure at altitude of h2
+    Pd = 0.5 * rho_a_h * 50 ** 2  # dynamic pressure
+    d_p_gas = 2 * Pd  # pressure diff ( twice dynamic press) [kg/m**2]
+    d_T_gas = temp  # temp diff [K]
+    T_a = ISA[0]  # ambient temp [K]
+
+    lift = (g0 * (rho_a_h - ((p_gas_h + d_p_gas) / (R_gas * (T_a + d_T_gas))))) * V
+
+    return lift
+
+
+
+
+h1 = []
+h2 = []
+Vp = []
+L = []
+
+
+for j in range(101):
+    h = j * 200
+    V_p = Volume_calc(h, 30)
+    h1.append(h)
+    Vp.append(V_p)
+
+for i in range(101):
+    h = i *200 +10000
+    up = Lift_calc(h, 60000, 0)/ g0
+    L.append(up)
+    h2.append(h)
+
+
+
+
+
+# plt.plot(h1, Vp)
+plt.plot(h2, L)
+plt.show()
+
