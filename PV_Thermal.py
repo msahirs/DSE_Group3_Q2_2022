345e304d4619456b28e28626c88299c6c21cb108
WouterOffringa <wouter.offringa@outlook.com>
2023-01-11 11:55:31 +0100

    update to include heat disposal at bottom of solar panels

--- PV_Thermal.py
+++ PV_Thermal.py
@@ -11,8 +11,7 @@ A = 2700                # Area of solar cell configuration [m^2]
 h = 20000.0             # Height [m]
 v_wind = 5              # Wind speed [m/s]
 albedo = 0.1            # Earth's albedo [-]
-# I_sun = 1353            # Incident solar intensity [W/m^2]
-step = 7200            # Step size
+step = 7200             # Step size
 
 # Constants
 Cp = 1005                   # Specific heat capacity of air [J/kg/K]
@@ -31,7 +30,6 @@ Cp_module = Rho_Ge * Cp_Ge * d_Ge * A   # Heat capacity of PV [J/K]
 
 # Backplane (CFRP)
 Cp_cfrp = 1040              # Specific heat capacity of CFRP [J/kg/K]
-d_cfrp = 100 * 10 ** (-6)   # Thickness CFRP [m]
 k_cfrp_hor = 250            # Conductivity (hor) of CFRP [W/m/K]
 k_cfrp_ver = 3              # Conductivity (ver) of CFRP [W/m/K]
 epsilon_cfrp = 0.88         # CFRP emission coefficient [-]
@@ -57,35 +55,13 @@ I_new = 1.6*np.interp(n_list, tstep_list, I_list)
 dt = 1
 t_list = []
 T = T_air
-T_list = []
 T_cfrp = T_air
-temp = []
-q_abs_list = []
-q_forced_conv_list = []
-q_free_conv_list = []
-q_emission_list = []
+T_list = []
+q = [[],[],[],[],[],[],[]] # list of all heat flows: absorption, emission, free, forced, emission, free, forced
 
 for t in range(0, step, dt):
     t_list.append(t)
     T_list.append(T - 273.15)
-
-    # forced convection (turbulent flow)
-    Re = v_wind * rho * L_charac / mu
-    Nu_forced = 0.037 * Re ** (4 / 5) * Pr ** (1 / 3)
-    h_forced = Nu_forced * k / L_charac
-    q_forced_conv = h_forced * A * (T - T_air)
-    q_forced_conv_list.append(q_forced_conv)
-
-    # free convection
-    Ra = g * beta / nu / alpha * (T - T_air) * L_charac ** 3
-    Nu_free = 0.15 * Ra ** (1 / 3)
-    h_free = Nu_free * k / L_charac
-    q_free_conv = h_free * A * (T - T_air)
-    q_free_conv_list.append(q_free_conv)
-
-    # emission radiation
-    q_emission = sigma * epsilon * (T ** 4 - T_air ** 4) * A
-    q_emission_list.append(q_emission)
 
     # direct absorption (from sun & earth)
     alpha_ab = 0.65  # TBD
@@ -93,41 +69,64 @@ for t in range(0, step, dt):
     I_ab = I_new[t] * (1 - refl_top)
     # I_ab = (I_sun * (1+albedo) + I_earth) * (1 - refl_top)
     q_abs = I_ab * alpha_ab * A
-    q_abs_list.append(q_abs)
+    q[0].append(q_abs)
+
+    # emission radiation
+    q_emission = sigma * epsilon * (T ** 4 - T_air ** 4) * A
+    q[1].append(q_emission)
+
+    # free convection
+    Ra = g * beta / nu / alpha * (T - T_air) * L_charac ** 3
+    Nu_free = 0.15 * Ra ** (1 / 3)
+    h_free = Nu_free * k / L_charac
+    q_free_conv = h_free * A * (T - T_air)
+    q[2].append(q_free_conv)
+
+    # forced convection (turbulent flow)
+    Re = v_wind * rho * L_charac / mu
+    Nu_forced = 0.037 * Re ** (4 / 5) * Pr ** (1 / 3)
+    h_forced = Nu_forced * k / L_charac
+    q_forced_conv = h_forced * A * (T - T_air)
+    q[3].append(q_forced_conv)
 
     # conduction to backplane
     T_cfrp = T
+    T_bottom = T_air
 
     # CFRP emission
-    q_emission_cfrp = sigma * epsilon_cfrp * (T ** 4 - T_air ** 4) * A
+    q_emission_cfrp = sigma * epsilon_cfrp * (T ** 4 - (T_bottom) ** 4) * A
+    q[4].append(q_emission_cfrp)
+
+    # CFRP free convection air layer
+    Ra_cfrp = g * beta / nu / alpha * (T_cfrp - T_bottom) * L_charac ** 3
+    Nu_cfrp_free = 0.15 * Ra_cfrp ** (1 / 3)
+    h_cfrp_free = Nu_cfrp_free * k / L_charac
+    q_cfrp_free_conv = h_cfrp_free * A * (T_cfrp - T_bottom)
+    q[5].append(q_cfrp_free_conv)
 
     # CFRP forced convection air layer (turbulent flow)
     Pr_cfrp = Pr
     Re_cfrp = Re
     Nu_cfrp_forced = 0.037 * Re_cfrp ** (4 / 5) * Pr_cfrp ** (1 / 3)
     h_cfrp_forced = Nu_cfrp_forced * k / L_charac
-    q_cfrp_forced_conv = h_cfrp_forced * A * (T_cfrp - T_air)
-
-    # CFRP free convection air layer
-    Ra_cfrp = g * beta / nu / alpha * (T_cfrp - T_air) * L_charac ** 3
-    Nu_cfrp_free = 0.15 * Ra_cfrp ** (1 / 3)
-    h_cfrp_free = Nu_cfrp_free * k / L_charac
-    q_cfrp_free_conv = h_cfrp_free * A * (T_cfrp - T_air)
+    q_cfrp_forced_conv = h_cfrp_forced * A * (T_cfrp - T_bottom)
+    q[6].append(q_cfrp_forced_conv)
 
     # Time step
-    dT = (q_abs - q_forced_conv - q_free_conv - q_emission) / Cp_module * dt
+    dT = (q_abs - q_forced_conv - q_free_conv - q_emission - q_emission_cfrp - q_cfrp_free_conv - q_cfrp_forced_conv) / Cp_module * dt
     T = T + dT
-# - q_emission_cfrp - q_cfrp_free_conv - q_cfrp_forced_conv
 
 # Finding maxima and printing
 max_index = np.argmax(T_list)
 print("\nMax temperature: ", T_list[max_index])
 print("At this point in time the heat balance consists of:")
-print(round(q_abs_list[max_index]), "Watts absorbed")
-print(round(q_forced_conv_list[max_index]), "Watts released trough forced conv", round(q_forced_conv_list[max_index]/q_abs_list[max_index],2), "%")
-print(round(q_free_conv_list[max_index]), "Watts released trough free conv", round(q_free_conv_list[max_index]/q_abs_list[max_index],2), "%")
-print(round(q_emission_list[max_index]), "Watts released through radiation", round(q_emission_list[max_index]/q_abs_list[max_index],2), "%")
-
+print(round(q[0][max_index]), "Watts absorbed")
+print(round(q[1][max_index]), "Watts released through top radiation", round(q[1][max_index]/q[0][max_index],2), "%")
+print(round(q[2][max_index]), "Watts released trough top free conv", round(q[2][max_index]/q[0][max_index],2), "%")
+print(round(q[3][max_index]), "Watts released trough top forced conv", round(q[3][max_index]/q[0][max_index],2), "%")
+print(round(q[4][max_index]), "Watts released through bottom radiation", round(q[4][max_index]/q[0][max_index],2), "%")
+print(round(q[5][max_index]), "Watts released trough bottom free conv", round(q[5][max_index]/q[0][max_index],2), "%")
+print(round(q[6][max_index]), "Watts released trough bottom forced conv", round(q[6][max_index]/q[0][max_index],2), "%")
 
 # Plotting
 fig, ax1 = plt.subplots()
@@ -164,7 +163,11 @@ for i in range(len(T_list)):
         Power_list.append(I_new[i] * (100 + PVIV.iv(T_list[i])[0])/100)
     else:
         Power_list.append(0)
-
+"""
 color = 'tab:red'
 ax2.plot(t_list, Power_list, color=color)
 plt.show()
+"""
+maxpower_index = np.argmax(Power_list)
+print(Power_list[maxpower_index])
+print(I_new[maxpower_index])
