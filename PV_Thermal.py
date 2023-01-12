a71b68ade94f08a78d5d01d1162c77652f9fec18
JelteB <jeltejb@gmail.com>
2023-01-12 09:57:31 +0100

    Now with Air layer heating

--- PV_Thermal.py
+++ PV_Thermal.py
@@ -6,9 +6,9 @@ import datetime
 import PVIV
 
 
-# Input Values
-L_charac = 52  # Characteristic length [m]
+# Input Variables
 A = 2700  # Area of solar cell configuration [m^2]
+dh = 1 # Distance between pv array and balloon [m]
 h = 20000.0  # Height [m]
 v_wind = 5  # Wind speed [m/s]
 albedo = 0.1  # Earth's albedo [-]
@@ -20,28 +20,33 @@ g = 9.81  # Gravitational acceleration [m/s^2]
 sigma = 5.67 * 10 ** (-8)  # Boltzmann constant [W/m^2/K^4]
 I_earth = 237  # Earth's infrared emission [W/m^2]
 
-# Solar cell (Germanium)
-epsilon = 0.9  # Ge substrate emission coefficient [-]
-refl_top = 0.02  # Reflectivity of solar cell cap [-]
-Rho_Ge = 5323  # Ge substrate density [kg/m^3]
-Cp_Ge = 3200  # Specific heat capacity of  Ge [J/kg/K]
-d_Ge = 160 * 10 ** (-6)  # Approx thickness Ge substrate [m]
-Cp_module = Rho_Ge * Cp_Ge * d_Ge * A  # Heat capacity of PV [J/K]
-
-# Backplane (CFRP)
-Cp_cfrp = 1040  # Specific heat capacity of CFRP [J/kg/K]
-k_cfrp_hor = 250  # Conductivity (hor) of CFRP [W/m/K]
-k_cfrp_ver = 3  # Conductivity (ver) of CFRP [W/m/K]
-epsilon_cfrp = 0.88  # CFRP emission coefficient [-]
-
-# General Calculations
+# Air Calculations
 T_air, P, rho, mu = ISA_general.ISA(h)
 nu = mu / rho  # Kinematic viscosity [m^2/s]
 Pr = mu * Cp / k  # Prantl number [-]
 alpha = k / rho / Cp  # Thermal diffusivity of air [m^2/s]
 beta = 1 / T_air  # Thermal expansion coefficient [1/K] (approx)
+Cp_bottom = Cp * A * dh * rho
+L_charac = 2 * np.sqrt(A/np.pi)  # Characteristic length [m]
+R_charac = L_charac / 2
+A_segment_ps = 0.5 * R_charac * 2 * np.degrees(np.arccos((R_charac - v_wind) / R_charac))
+New_air_ps = A_segment_ps / A
 
-# Data initialization (temporary)
+
+# Solar cell constants and calculations (Germanium)
+epsilon = 0.9  # Ge substrate emission coefficient [-]
+refl_top = 0.02  # Reflectivity of solar cell cap [-]
+Rho_Ge = 5323  # Ge substrate density [kg/m^3]
+Cp_Ge = 3200  # Specific heat capacity of  Ge [J/kg/K]
+d_Ge = 75 * 10 ** (-6)  # Approx thickness Ge substrate [m]
+Cp_module = Rho_Ge * Cp_Ge * d_Ge * A  # Heat capacity of PV [J/K]
+
+# Backplane constants and calculations (CFRP)
+Cp_cfrp = 1040  # Specific heat capacity of CFRP [J/kg/K]
+k_cfrp_hor = 250  # Conductivity (hor) of CFRP [W/m/K]
+k_cfrp_ver = 3  # Conductivity (ver) of CFRP [W/m/K]
+epsilon_cfrp = 0.88  # CFRP emission coefficient [-]
+
 
 
 # Initializing
@@ -57,19 +62,19 @@ time_flux = np.arange(0, 86400, time_step)
 flux_interp = np.interp(t_list, time_flux, flux)
 
 T = T_air
-T_cfrp = T_air
 T_list = []
+T_bottom = T_air
+T_bottom_list = []
 q = [[], [], [], [], [], [], []]  # list of all heat flows: absorption, emission, free, forced, emission, free, forced
 
 for t in t_list:
     T_list.append(T - 273.15)
+    T_bottom_list.append(T_bottom - 273.15)
     I_sun = flux_interp[t]
 
     # direct absorption (from sun & earth)
     alpha_ab = 0.65  # TBD
-    #    I_ab = I_sun * (1 - refl_top)
     I_ab = I_sun * (1 - refl_top)
-    # I_ab = (I_sun * (1+albedo) + I_earth) * (1 - refl_top)
     q_abs = I_ab * alpha_ab * A
     q[0].append(q_abs)
 
@@ -91,18 +96,16 @@ for t in t_list:
     q_forced_conv = h_forced * A * (T - T_air)
     q[3].append(q_forced_conv)
 
-    # conduction to backplane
-    T_bottom = T_air
 
     # CFRP emission
-    q_emission_cfrp = sigma * epsilon_cfrp * (T ** 4 - T_bottom ** 4) * A
+    q_emission_cfrp = sigma * epsilon_cfrp * (T ** 4 - T_bottom ** 4) * (A * 0.9)
     q[4].append(q_emission_cfrp)
 
     # CFRP free convection air layer
     Ra_cfrp = g * beta / nu / alpha * (T - T_bottom) * L_charac ** 3
     Nu_cfrp_free = 0.15 * Ra_cfrp ** (1 / 3)
     h_cfrp_free = Nu_cfrp_free * k / L_charac
-    q_cfrp_free_conv = h_cfrp_free * A * (T - T_bottom)
+    q_cfrp_free_conv = h_cfrp_free * (A * 0.9) * (T - T_bottom)
     q[5].append(q_cfrp_free_conv)
 
     # CFRP forced convection air layer (turbulent flow)
@@ -110,12 +113,16 @@ for t in t_list:
     Re_cfrp = Re
     Nu_cfrp_forced = 0.037 * Re_cfrp ** (4 / 5) * Pr_cfrp ** (1 / 3)
     h_cfrp_forced = Nu_cfrp_forced * k / L_charac
-    q_cfrp_forced_conv = h_cfrp_forced * A * (T - T_bottom)
+    q_cfrp_forced_conv = h_cfrp_forced * (A * 0.9) * (T - T_bottom)
     q[6].append(q_cfrp_forced_conv)
 
     # Time step
     dT = (q_abs - q_forced_conv - q_free_conv - q_emission - q_emission_cfrp - q_cfrp_free_conv - q_cfrp_forced_conv) / Cp_module * dt
     T = T + dT
+    dT_bottom = (q_emission_cfrp + q_cfrp_free_conv + q_cfrp_forced_conv) / Cp_bottom * dt
+    T_bottom = (T_bottom + dT_bottom) * (1-New_air_ps) + T_air * New_air_ps
+
+    #print(T, dT, T_bottom, dT_bottom, Ra_cfrp, q_emission_cfrp)
 
 
 # Finding maxima and printing
@@ -166,7 +173,9 @@ plt.xticks(t_list[initial_time::div], labels)
 '''
 fig.tight_layout()
 
-print(PVIV.iv(np.amax(T_list))[0])
+print(PVIV.iv(np.amax(T_list))[0], 'mpp eff loss')
+print(np.amax(flux_interp), 'mpp flux')
+print(np.where(flux_interp == np.amax(flux_interp)), 'mpp time')
 plt.show()
 
 
