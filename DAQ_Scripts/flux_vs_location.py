c38133bf3c6e4217f2fa9ed48684cde1ee0a309d
ssujahudeen <sujahudeen.sahir@gmail.com>
2023-01-16 15:06:56 +0100

    Added plots for visualisation of considered locs

--- DAQ_Scripts/flux_vs_location.py
+++ DAQ_Scripts/flux_vs_location.py
@@ -16,6 +16,9 @@ net_fluxes = []
 
 mypath = "data/input_archive/relev_locs/merged_all_*.csv"
 
+fig1, ax1= plt.subplots()
+fig2, ax2= plt.subplots()
+
 for filename in glob.glob(mypath):
 
     code = filename[-6:-4]
@@ -43,22 +46,32 @@ for filename in glob.glob(mypath):
 
     y_axis = f_data.iloc[:,-3]
 
-    plt.plot( x_axis, y_axis, 'o',markersize = 3, label = filename[-6:-4], alpha = 0.8)
+    ax1.plot( x_axis, y_axis, 'o',markersize = 3, label = filename[-6:-4], alpha = 0.8)
 
-    break
+    
 
 # vert_csv = pd.concat(list_csvs)
 
-ax = plt.gcf().axes[0] 
-ax.xaxis.set_major_formatter(formatter)
-plt.legend()
-plt.gcf().autofmt_xdate(rotation=25)
-plt.xlabel("Time")
-plt.ylabel("Solar Irradiance")
-plt.show()
+ax1.xaxis.set_major_formatter(formatter)
+ax1.legend()
+fig1.autofmt_xdate(rotation=20)
+ax1.set_xlabel("Time")
+ax1.tick_params(axis='x', labelsize = 9)
+ax1.set_ylabel("Solar Irradiance")
+ax1.set_title("Variation of solar irradiance over 2022 at 18 km, for considered locations")
+fig1.set_figheight(7)
+fig1.set_figwidth(13)
+fig1.savefig("data/input_archive/relev_locs/Irr_vs_time_locs.png")
+# plt.savefig("data/input_archive/relev_locs/Irr_vs_time_locs.png")
 
-plt.bar(codes,net_fluxes)
+
+ax2.bar(codes,net_fluxes)
 # plt.xlabel("Time")
-plt.ylabel("Average Solar Irradiance at computed times [W/m^2]")
+ax2.set_ylabel("Solar Irradiance[W/m^2]")
+ax2.set_xlabel("Region Codes")
+ax2.set_title("Average solar irradiance for 2022 at 18 km, for considered locations")
+fig2.set_figheight(7)
+fig2.set_figwidth(13)
+fig2.savefig("data/input_archive/relev_locs/avg_Irr_vs_locs.png")
 
 plt.show()
