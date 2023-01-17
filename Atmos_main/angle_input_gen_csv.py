4b055bf3851f81ca3fb8af7c0b3436368b3fa74f
ssujahudeen <sujahudeen.sahir@gmail.com>
2023-01-17 09:51:09 +0100

    Added zero elev input files for geographic sweep

--- Atmos_main/angle_input_gen_csv.py
+++ Atmos_main/angle_input_gen_csv.py
@@ -15,30 +15,30 @@ ref_angle_inp = "2022 7 15 13 30 0 0 0 69.184 120 30 0 1013.25 15 0 0 0.65 1".sp
 ref_angle_inp = [float(x) for x in ref_angle_inp]
 ref_angle_inp = np.array(ref_angle_inp)
 
-second_sweep = np.arange(start = 0, stop = 60, step= 30)
-minute_sweep = np.arange(start = 0, stop = 60, step= 1)
+# second_sweep = np.arange(start = 0, stop = 60, step= 30)
+minute_sweep = np.arange(start = 0, stop = 60, step= 10)
 hour_sweep = np.arange(0,24,1)
-# day_sweep = np.arange(1,29,1)
-# month_sweep = np.arange(1,13,1)
-elevation_sweep = np.arange(12000,21000,500)
+day_sweep = np.arange(1,29,1)
+month_sweep = np.arange(1,13,1)
+# elevation_sweep = np.arange(12000,21000,500)
 # longitude_sweep = np.arange(-180,181,20)
 # latitude_sweep = np.arange(-90,91,10)
 
-longitude_sweep = [3.84898]
+longitude_sweep = [21.17911]
 
-latitude_sweep = [52.37079]
+latitude_sweep = [37.36599]
 
 # slope_sweep = np.arange(-175,180,5)
 
-# sweep_params = np.array(np.meshgrid(hour_sweep,minute_sweep,day_sweep,month_sweep,longitude_sweep,latitude_sweep,copy=False)).T.reshape(-1,6)
+sweep_params = np.array(np.meshgrid(hour_sweep,minute_sweep,day_sweep,month_sweep,longitude_sweep,latitude_sweep,copy=False)).T.reshape(-1,6)
 # sweep_params = np.array(np.meshgrid(hour_sweep,minute_sweep,day_sweep,month_sweep,elevation_sweep)).T.reshape(-1,5)
-sweep_params = np.array(np.meshgrid(hour_sweep,minute_sweep,second_sweep,elevation_sweep)).T.reshape(-1,4)
+# sweep_params = np.array(np.meshgrid(hour_sweep,minute_sweep,second_sweep,elevation_sweep)).T.reshape(-1,4)
 # sweep_params = np.array(np.meshgrid(slope_sweep)).T.reshape(-1,1)
 
 main_data = np.tile(ref_angle_inp, (sweep_params.shape[0],1))
 
-pressures = [isa.getVals(h)[0]/100 for h in sweep_params[:,3]]
-temperatures = [isa.getVals(h)[1]/100 for h in sweep_params[:,3]]
+# pressures = [isa.getVals(h)[0]/100 for h in sweep_params[:,3]]
+# temperatures = [isa.getVals(h)[1]/100 for h in sweep_params[:,3]]
 
 # print(sweep_params)
 # print(main_data.shape)
@@ -48,12 +48,12 @@ main_data[:,3] = sweep_params[:,0]
 main_data[:,4] = sweep_params[:,1]
 main_data[:,5] = sweep_params[:,2]
 main_data[:,11] = sweep_params[:,3]
-main_data[:,12] = pressures
-main_data[:,13] = temperatures
+# main_data[:,12] = pressures
+# main_data[:,13] = temperatures
 
 
-# main_data[:,9] = sweep_params[:,4]
-# main_data[:,10] = sweep_params[:,5]
+main_data[:,9] = sweep_params[:,4]
+main_data[:,10] = sweep_params[:,5]
 
 # main_data[:,14] = sweep_params[:,0]
 
@@ -61,7 +61,7 @@ main_data[:,13] = temperatures
 if not os.path.exists(os.path.join(curr_dir,"data")):
     os.mkdir("data")
 
-np.savetxt("./data/input_archive/elevation_gain/sweep_parameters.csv", main_data, fmt = "%.2f", delimiter= ","
+np.savetxt("./data/input_archive/relev_locs_zero/sweep_parameters_ME.csv", main_data, fmt = "%.2f", delimiter= ","
             , header=','.join(REF_ANGLE_HEADERS))
 
 print("Written Input sweep data")
