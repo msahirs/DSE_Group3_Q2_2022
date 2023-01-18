e4408b2c7c911391bd1f8aa24982fc27977a9069
WouterOffringa <wouter.offringa@outlook.com>
2023-01-18 09:12:11 +0100

    various updates from yesterday

--- Compression_rod.py
+++ Compression_rod.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from Payload import distribution1
 
 def rod_load (F_tension, W_payload, safety_f, tensile_str, density):
     tensile_str_all=(1-safety_f)*tensile_str
@@ -20,17 +21,16 @@ def rod_load (F_tension, W_payload, safety_f, tensile_str, density):
 
     return m_rod, radius, A
 
-
 # Constants
 g=9.81
 yield_str = 276 * 10 ** 6  # Pa, when is the same for tension and compression
 tensile_str=yield_str
 compress_str=yield_str
 density=2700
-L_rod = 21 # m
+L_rod = 19.5 # m
 
 # Input
-F_tension = 12000 # N
+F_tension = 24000 # N
 W_payload = 1500 # N
 safety_f = 0.25
 
@@ -51,35 +51,34 @@ ri=0.03
 r0=math.sqrt(A/3.14+ri**2)
 print(r0)
 print("Carbon composite rod: mass ", m_rod,"kg, radius ", radius,"m, cross-sectional area ", A, "m^3")
-
+"""
 
 # I-Beam calculations
 sigma_max = (1-safety_f) * yield_str
 tau_max = (1-safety_f) * (yield_str / 2)
 
-# Inputs
-F_down = 1534
-x_down = 19.98
-F_up = 0.52 * F_down
-x_up = 27
-F_up2 = 0.52 * F_down
-x_up2 = 12
+length = 29.3
+x_down, F_down, F_tot, w = distribution1(4.5, 1000, length, 24)
 
-F_0 = F_up + F_up2 - F_down
-M_0 = F_up * x_up + F_up2 * x_up2 - F_down * x_down
-print(F_0, M_0)
+F_0 = 0.01* F_down
+M_0 = F_down * length - 1.4* F_down * x_down
 
-tau_lim, sigma_lim = 0,0
-t = 0.01
+print("Downwards force is:", F_down)
+print("Force and moment at hinge are:", F_0, M_0)
+
+tau_lim, sigma_lim, d = 0, 0, 0
+t = 0.1
 
 while tau_lim <= tau_max and sigma_lim <= sigma_max:
     d = 100 * t
-    tau_lim = 9/16 * F_0 / t*d
+    tau_lim = 9/16 * F_0 / (t*d)
     sigma_lim = 3/8 * M_0 / (t*d*d)
     t = t - 0.0000001
 
+
 A = 4 * d * t
-W_I = A * x_up * 2700
+W_I = A * length * 2700
 
 print(t, d, A, W_I)
 print(tau_lim, sigma_lim)
+"""
---
+++ Payload.py
@@ -0,0 +1,110 @@
+import math as m
+import matplotlib.pyplot as plt
+import numpy as np
+
+# angleused = 2*m.pi / beams
+
+def distribution1(dis_force, n, length, beams):
+    w = []
+    w_i = 0
+    circum = 2*np.pi*length
+    max_width = circum / beams
+    x_eq = 2/3 * length
+    F_eq = 1/2 * length * max_width * dis_force
+    F_tot = dis_force * np.pi * length**2
+
+    for i in range(n):
+        w_i = w_i + (dis_force * max_width)/n
+        w.append(w_i)
+
+    return x_eq, F_eq, F_tot, w
+
+def distribution2(dis_force, n, length, beams):
+    w = []
+    w_i = 0
+    circum = 2*np.pi*length
+    max_width = circum / beams
+    x_eq = 1/2 * length
+    F_eq = 1/2 * length * max_width * dis_force
+    F_tot = dis_force * np.pi * length**2
+
+    for i in range(n):
+        w_i = w_i + (dis_force * max_width)/n
+        w.append(w_i)
+
+    return x_eq, F_eq, F_tot, w
+
+# print(distribution1(9.75,1000,40,24))
+
+"""
+Length = 29.
+Fullarea = np.pi * Length**2
+
+density_solarpanels = 0.45
+r = m.sqrt(Fullarea/m.pi)\
+
+a = m.sqrt(r**2 + r**2 - (2*r*r*m.cos(angleused)))
+
+b = m.sqrt(r**2 - (a/2)**2 )
+
+d = r-b
+
+percentage = (d+b)/100
+b_per = b/percentage / 100
+d_per = d/percentage / 100
+
+x1 = np.linspace(0,b_per*r,92)
+x2 = np.linspace(b_per*r,r,8)
+
+densitynewtons = 4.5 * 9.81 * (r/100) * 2
+
+
+m1 = x1 * (a/2/b)
+m2 = np.sqrt(r**2-x2**2)
+
+w1 = m1 * densitynewtons
+
+w2 = m2 * densitynewtons
+
+xmax = b
+mmax = xmax * (a/2/b)
+wmax = mmax * densitynewtons
+EqF1mag = wmax*xmax*0.5
+print("Equivalent load of the distributed load w1:", EqF1mag)
+
+EqF1loc = (2/3) * b
+print("Location of equivalent load of the distributed load w1:", EqF1loc)
+
+
+r = 29.316150714175198
+x21 = 0
+x22 = d
+
+int21w2 = densitynewtons * 0.5 * ((x21 * np.sqrt(r**2-x21**2) + (r**2 * m.atan(np.sqrt(r**2-x21**2)))))
+int22w2 = densitynewtons * 0.5 * (x22 * np.sqrt(r**2-x22**2) + (r**2 * m.atan(np.sqrt(r**2-x22**2))))
+intw2 = (int22w2 - int21w2)
+
+
+print("Equivalent load of the distributed load w2:", intw2)
+
+
+loc21w2 = -1/3 * ((r**2-x21**2)**(3/2))
+loc22w2 = -1/3 * ((r**2-x22**2)**(3/2))
+
+loc = (loc22w2 - loc21w2) / intw2
+
+
+
+locexact =  b +loc
+print("Location of equivalent load of the distributed load w2:", locexact)
+
+magnituderesultantforce = intw2 + EqF1mag
+print("Magnitude resulting force:", magnituderesultantforce)
+
+locationresultantforce = ((locexact * intw2) + (EqF1loc* EqF1mag))/magnituderesultantforce
+print("Location resulting force:", locationresultantforce)
+
+plt.plot(x1, w1, 'r')
+plt.plot(x2, w2, 'b')
+plt.show()
+"""
